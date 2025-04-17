519a067c619244b35d34ccd073968d7727764232
MarcusDeslandes <marcus_deslandes@hotmail.com>
2025-04-17 15:59:31 -0300

    textarea alterado e script

--- script.js
+++ script.js
@@ -14,20 +14,3 @@ botaoFormulario.addEventListener('click', () => {
     formulario.className = 'edit';
     formulario.innerHTML = 'Obrigado! Responderei assim que possível!';
 })
-
-//carrossel
-let carrosseis = document.getElementsByClassName('projetos-carrossel');
-
-for(let i = 0; i < carrosseis.length; i++) {
-    let carrossel = carrosseis[i];
-
-    let itens = carrossel.getElementsByClassName('projetos-carrossel-item');
-    let posicaoAnterior = 0;
-    let mover = posicaoAnterior + 100
-
-    for(let c = 0; c < itens.length; c++) {
-        itens[c].style.right = mover + '%'
-
-        posicaoAnterior = mover
-    }
-}
