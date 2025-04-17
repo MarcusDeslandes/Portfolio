bf46ec7e31379fa47b862e757acceb3e0ec6815c
MarcusDeslandes <marcus_deslandes@hotmail.com>
2025-04-17 15:53:06 -0300

    Versão mobile pronta

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ const menu = document.getElementById('menu');
 hamburguer.addEventListener('click', () => {
     menu.classList.toggle('active')
 })
+
+// Formulario
+const formulario = document.querySelector('#formulario');
+const botaoFormulario = document.querySelector('#formulario-botao');
+
+botaoFormulario.addEventListener('click', () => {
+    formulario.className = 'edit';
+    formulario.innerHTML = 'Obrigado! Responderei assim que possível!';
+})
+
+//carrossel
+let carrosseis = document.getElementsByClassName('projetos-carrossel');
+
+for(let i = 0; i < carrosseis.length; i++) {
+    let carrossel = carrosseis[i];
+
+    let itens = carrossel.getElementsByClassName('projetos-carrossel-item');
+    let posicaoAnterior = 0;
+    let mover = posicaoAnterior + 100
+
+    for(let c = 0; c < itens.length; c++) {
+        itens[c].style.right = mover + '%'
+
+        posicaoAnterior = mover
+    }
+}
